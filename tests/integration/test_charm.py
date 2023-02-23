aa517b4c4ae837f2ce9d574bb850a5fb6aa7c20f
natalia <natalianowakowska@mail.com>
2023-02-23 15:09:38 +0100

    add trust in integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -42,6 +42,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
         entity_url=charm,
         resources={"oci-image": hydra_image_path},
         series="jammy",
+        trust=True,
     )
 
     await ops_test.model.add_relation(
