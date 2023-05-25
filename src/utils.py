4f2bf6ea032a737fb9727613c5ae7c5b318f270c
Nikos <nikos.sklikas@canonical.com>
2023-05-25 11:52:29 +0300

    Update docstring

--- src/utils.py
+++ src/utils.py
@@ -14,16 +14,22 @@ def remove_none_values(dic: Dict) -> Dict:
 
 
 def normalise_url(url: str) -> str:
-    """Convert a URL to a more userfriendly format.
+    """Convert a URL to a more user friendly HTTPS URL.
 
     The user will be redirected to this URL, we need to use the https prefix
     in order to be able to set cookies (secure attribute is set). Also we remove
     the port from the URL to make it more user-friendly.
 
+    For example:
+        http://ingress:80 -> https://ingress
+        http://ingress:80/ -> https://ingress/
+        http://ingress:80/path/subpath -> https://ingress/path/subpath
+
     This conversion works under the following assumptions:
     1) The ingress will serve https under the 443 port, the user-agent will
        implicitly make the request on that port
     2) The provided URL is not a relative path
+    3) No user/password is provided in the netloc
 
     This is a hack and should be removed once traefik provides a way for us to
     request the https URL.
