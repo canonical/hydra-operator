e7af7f2d5b8ae547e7fb6698d5537e42bbeca5d3
dushu <shu.du@canonical.com>
2024-07-21 18:30:01 -0400

    refactor: refactor command line

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,10 @@
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""File containing all constants."""
+# Application constants
+PUBLIC_PORT = 4444
+ADMIN_PORT = 4445
+CONFIG_FILE_NAME = "/etc/config/hydra.yaml"
 
+# Integration constants
 INTERNAL_INGRESS_RELATION_NAME = "internal-ingress"
